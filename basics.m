efe37e8801fb125ffccf2959ee58674e6b0ee183
Sidhartha Singh <15666676+sidharthasingh@users.noreply.github.com>
2017-11-27 09:01:39 +0530

    Old merger

---
+++ basics.m
@@ -0,0 +1,25 @@
+10^2 // 10 raised to power
+
+ones(2,3) // A 2x3 matric of 1s
+
+size(ones(3,4)) // [3,4]
+
+v = [1;2;3;4] /*1
+				2
+				3
+				4*/
+a = 5
+c = v + a*ones(size(v)) // Is same as
+c = v + a
+
+v^2 // v*v
+v.^2 // v[i]^2 i=>{1..n}
+v.^w
+
+v.*x
+
+// v' Transpose
+
+v = 2:0.2:3 // [2 2.2 2.4 2.6 2.8 3]
+v = 2:5 // [2 3 4 5]
+
